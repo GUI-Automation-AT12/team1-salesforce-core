e299096d292e7b09760df43f6a8b88bd812e26b6
Samuel Loza <starsaminf@gmail.com>
2021-01-12 09:21:13 -0400

    Add ConstansCore class

---
+++ src/main/java/org/fundacionjala/core/constans/ConstantsCore.java
@@ -0,0 +1,26 @@
+package org.fundacionjala.core.constans;
+
+/**
+ * [SL] This class offers constant for the core.
+ */
+public final class ConstantsCore {
+    /**
+     * Constructor.
+     */
+    private ConstantsCore() {
+
+    }
+    /** Navigator Constants. **/
+    public static final String BROWSER_CHROME = "Google Chrome";
+    public static final String BROWSER_CHROMIUM = "Chromium";
+    public static final String BROWSER_MOZILLA = "Mozilla Firefox";
+    public static final String BROWSER_EDGE = "Microsoft Edge";
+
+    /** Browser Properties Constants. **/
+    public static final String BROWSER_VERSION = "version";
+    public static final String BROWSER_IMPLICITWAIT = "implicitWaiting";
+    public static final String BROWSER_EXPLICITWAIT = "explicitWaiting";
+    public static final String BROWSER_SLEEPINGTIME = "sleepingTime";
+    public static final String BROWSER_FLAGS = "flags";
+
+}
