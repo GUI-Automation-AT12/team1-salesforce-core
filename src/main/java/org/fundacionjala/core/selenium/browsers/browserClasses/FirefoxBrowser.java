f5a0d561c1c9541d7314446936c3d53fe4816440
Samuel Loza <starsaminf@gmail.com>
2021-01-23 04:29:51 -0400

    Add support for flags and download path

--- src/main/java/org/fundacionjala/core/selenium/browsers/browserClasses/FirefoxBrowser.java
+++ src/main/java/org/fundacionjala/core/selenium/browsers/browserClasses/FirefoxBrowser.java
@@ -3,9 +3,11 @@ package org.fundacionjala.core.selenium.browsers.browserClasses;
 import static io.github.bonigarcia.wdm.DriverManagerType.FIREFOX;
 import io.github.bonigarcia.wdm.FirefoxDriverManager;
 import org.fundacionjala.core.constans.BrowserConstants;
+import org.fundacionjala.core.selenium.browsers.Browser;
 import org.fundacionjala.core.selenium.browsers.BrowserParser;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 /**
  * [MR] Class of Browser that returns a FirefoxDriver as WebDriver.
@@ -17,8 +19,10 @@ public class FirefoxBrowser implements IBrowser {
      */
     @Override
     public WebDriver initDriver() {
-        FirefoxDriverManager.getInstance(FIREFOX).version(
-                BrowserParser.getBrowsersMap().get(BrowserConstants.BROWSER_MOZILLA).getVersion()).setup();
-        return new FirefoxDriver();
+        Browser browser = BrowserParser.getBrowsersMap().get(BrowserConstants.BROWSER_MOZILLA);
+        FirefoxDriverManager.getInstance(FIREFOX).version(browser.getVersion()).setup();
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments(browser.getFlags());
+        return new FirefoxDriver(options);
     }
 }
